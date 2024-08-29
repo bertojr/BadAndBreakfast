27b3d24dd864cb2b72c5f57f219b4a5857f3b8cc
bertojr <1020bertone@gmail.com>
2024-08-29 15:18:35 +0200

    .

--- back-end/Services/AuthService.cs
+++ back-end/Services/AuthService.cs
@@ -11,7 +11,7 @@ namespace back_end.Services
 {
 	public class AuthService : IAuthService
 	{
-        private readonly JwtSettings _jwtSettings;
+        private readonly JwtSettings _jwtSettings; // contiene le impostazione JWT
         private readonly ApplicationDbContext _dbContext;
 
 		public AuthService(IOptions<JwtSettings> jwtSettings,
@@ -28,29 +28,41 @@ namespace back_end.Services
 
             if(user != null && email == user.Email && password == user.PasswordHash)
             {
-                var tokenHandler = new JwtSecurityTokenHandler(); // per creare e validare i token JWT
-                var Key = Encoding.ASCII.GetBytes(_jwtSettings.Key); // conversione della chiave in un array di byte
+                // creazione e gestione dei token JWT
+                var tokenHandler = new JwtSecurityTokenHandler();
+                // chiave segreta per firmare il token convertita in un array di byte
+                var Key = Encoding.ASCII.GetBytes(_jwtSettings.Key);
+                // descrive le caratteristiche del token JWT
                 var tokenDescriptor = new SecurityTokenDescriptor
                 {
+                    // identità dell'utente
                     Subject = new ClaimsIdentity(new[]
                     {
                         new Claim(ClaimTypes.Name, email),
                         new Claim(ClaimTypes.NameIdentifier, user.UserID.ToString())
                     }),
+                    // data di scadenza del token
                     Expires = DateTime.UtcNow.AddMinutes(_jwtSettings.DurationInMinutes),
+                    // credenziali per firmare il token
                     SigningCredentials = new SigningCredentials(
                         new SymmetricSecurityKey(Key),
                         SecurityAlgorithms.HmacSha256Signature),
+                    // emittente del token
                     Issuer = _jwtSettings.Issuer,
+                    // destinatario del token
                     Audience = _jwtSettings.Audience
                 };
 
+                // crea il token jwt sulla base del token descriptor
                 var token = tokenHandler.CreateToken(tokenDescriptor);
+                // converte il token in una stringa JWT
                 var jwtToken = tokenHandler.WriteToken(token);
 
+                // restituisco il bearer token e l'utente connesso
                 return (jwtToken, user);
             }
 
+            // se credenziali errate restituisco null
             return (null, null);
         }
 
