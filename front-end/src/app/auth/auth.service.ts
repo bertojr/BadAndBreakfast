18e8c6befee1407ea00a328b5614fcdc8314b42d
bertojr <1020bertone@gmail.com>
2024-09-04 11:55:28 +0200

    autenticazione part 1

--- front-end/src/app/auth/auth.service.ts
+++ front-end/src/app/auth/auth.service.ts
@@ -1,9 +1,54 @@
+import { iAuthData } from './../models/i-auth-data';
 import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { BehaviorSubject, map, Observable, tap } from 'rxjs';
+import { iUser } from '../models/i-user';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environments';
+import { iAuthResponse } from '../models/i-auth-response';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  // ci permette di lavora meglio con i JWT
+  jwtHelper: JwtHelperService = new JwtHelperService();
 
-  constructor() { }
+  // contiene l'oggetto user con tutte le info, se null vuol dire che non è loggato
+  authSubject = new BehaviorSubject<null | iUser>(null);
+
+  // uso questa variabile nelle guard e nell'interceptor per sapere se l'utente è loggoato o meno
+  syncIsLoggedIn: boolean = false;
+
+  // contiene i dati dell'utente loggato oppure null
+  user$ = this.authSubject.asObservable();
+
+  // restituiesce true se l'utente è loggato, false non è loggato
+  isLoggedIn$ = this.user$.pipe(
+    map((user) => !!user),
+    tap((user) => (this.syncIsLoggedIn = user))
+  );
+
+  constructor(private http: HttpClient) {}
+
+  loginUrl: string = `${environment.apiUrl}/auth/login`;
+  registerUrl: string = `${environment.apiUrl}/auth/register`;
+
+  // metodo per effettuare la registrazione
+  register(newUser: Partial<iUser>): Observable<iAuthResponse> {
+    return this.http.post<iAuthResponse>(this.registerUrl, newUser);
+  }
+
+  // metodo che effettua la chiamata per effettuare il login
+  login(authData: iAuthData): Observable<iAuthResponse> {
+    return this.http.post<iAuthResponse>(this.loginUrl, authData).pipe(
+      tap((data) => {
+        // quando l'utente è loggato comunico i suoi dati al subject
+        this.authSubject.next(data.user);
+
+        // salvo anche i suoi dati in localstorage
+        localStorage.setItem('accessData', JSON.stringify(data));
+      })
+    );
+  }
 }
--- front-end/src/app/auth/login/login.component.ts
+++ front-end/src/app/auth/login/login.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { iAuthData } from '../../models/i-auth-data';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  authData: iAuthData = {
+    email: 'admin@admin.com',
+    password: 'password',
+  };
 
+  constructor(private authSvc: AuthService) {}
+
+  login() {
+    this.authSvc.login(this.authData).subscribe(
+      (data) => {
+        console.log('Login successful:', data);
+      },
+      (error) => {
+        console.error('Error during login:', error);
+      }
+    );
+  }
 }
