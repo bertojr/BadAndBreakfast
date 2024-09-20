e3cabff50d35c19c551d19d1bdc15180e6b3e9e4
bertojr <1020bertone@gmail.com>
2024-09-20 11:55:39 +0200

    .

--- front-end/src/app/pages/home-page/booking-section/booking-section.component.ts
+++ front-end/src/app/pages/home-page/booking-section/booking-section.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { BookingService } from '../../../services/booking.service';
 
 @Component({
   selector: 'app-booking-section',
   templateUrl: './booking-section.component.html',
-  styleUrl: './booking-section.component.scss'
+  styleUrl: './booking-section.component.scss',
 })
 export class BookingSectionComponent {
+  checkInDate!: string;
+  checkOutDate!: string;
 
+  constructor(private router: Router, private bookingSvc: BookingService) {}
+
+  ngOnInit(): void {
+    const dates = this.bookingSvc.getDates();
+    this.checkInDate = dates.checkInDate;
+    this.checkOutDate = dates.checkOutDate;
+  }
+  search() {
+    if (this.checkInDate && this.checkOutDate) {
+      this.bookingSvc.setDates(
+        new Date(this.checkInDate),
+        new Date(this.checkOutDate)
+      );
+      this.router.navigate(['/bookings']);
+    }
+  }
 }
