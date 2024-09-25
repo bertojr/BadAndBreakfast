d0fdb8cb55aea471f8e09c6da317ef8554c4b4b8
bertojr <1020bertone@gmail.com>
2024-09-25 14:55:04 +0200

    modificata navbar graficamente

--- front-end/src/app/components/nav-bar/nav-bar.component.ts
+++ front-end/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { iUser } from '../../models/i-user';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.scss'
+  styleUrl: './nav-bar.component.scss',
 })
 export class NavBarComponent {
+  user!: iUser | null;
 
+  constructor(private authSvc: AuthService) {}
+
+  ngOnInit(): void {
+    this.authSvc.user$.subscribe((user) => {
+      this.user = user;
+    });
+  }
+
+  logout(): void {
+    this.authSvc.logout();
+  }
 }
